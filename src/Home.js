9841b768d53e6cfce26e93df53bdc64a7443e0c1
AnushkaPraveen <anushkapraveen98@gmail.com>
2022-02-18 15:40:23 +0530

    connect to local cms

--- src/Home.js
+++ src/Home.js
@@ -1,85 +1,97 @@
-import React, { useState } from "react";
-import {View,Text, Button,StyleSheet,ScrollView} from "react-native"
-import axios from "axios";
-import { useQuery } from "react-query";
+import React, {useState} from 'react';
+import {View, Text, Button, StyleSheet, ScrollView,Image,Card} from 'react-native';
+import axios from 'axios';
 
 
-const endpoint = "https://api.spacex.land/graphql/";
+const endpoint = 'http://10.0.2.2:8080/site/hmdb/master/hmdb/api';
 const GRAPHQL_QUERY = `
-  {
-    launchesPast(limit: 10) {
-      id
-      mission_name
-      rocket {
-        rocket_name
-        }
-    }
-    
+{
+  guillotine{
+    getChildren(key:"/hmdb/movies" first:50){
+          displayName
+          data : dataAsJson
+      }
   }
+}
 `;
 
-const Home =()=>{
-    const[data,setData]=useState([])
+const Home = () => {
+  const[data,setData]=useState([])
 
-    const fetchData = async () => {
-        await axios({
-          url: endpoint,
-          method: "POST",
-          data: {
-            query: GRAPHQL_QUERY
-          }
-        }).then(response => setData(response.data.data));
-        console.log(data);
-        
-      };
-
-    
-    
-    return(
-        <ScrollView >
-        
-        <Text style={styles.headline}>SpaceX Missions</Text>
-        {data.length==0?  <View style={styles.button}> 
-            <Button  title="get Data" onPress={fetchData}/> 
-          </View>:null}
-          
-           
-           
-           {data.length!= 0? <View>{data.launchesPast.map((launch) => (
-          <View style={styles.list} key={launch.id}><Text style={styles.list_text}style={styles.list_text}>Mission Name - <Text style={styles.sub_text}>{launch.mission_name} </Text></Text><Text style={styles.list_text}>Rocket Name - <Text style={styles.sub_text}>{launch.rocket.rocket_name}</Text></Text></View>
-        ))}</View> :null}
+  const fetchData = async () => {
+    await axios({
+      url: endpoint,
+      method: 'POST',
+      data: {
+        query: GRAPHQL_QUERY,
        
-           
-        </ScrollView>
-    )
-}
-const styles=StyleSheet.create({
-    headline:{
-       fontSize:40,
-       alignSelf:'center',
-       marginTop:40,
-       fontWeight:'bold',
-       color:'#00A5BF'      
-        
-    },
-    button:{
-padding:30,
+      },
+      /* headers:{
+            'Content-Type': 'application/json',
+           /*  'Access-Control-Allow-Origin': '*',
+            'access-control-allow-headers':'Content-Type',
+            'access-control-allow-methods':'POST,OPTIONS'
+      } */
+    }).then(response => setData(response.data.data.guillotine.getChildren));
+   
+  };
 
-alignItems:'center'
-    },
-    list:{
-        marginTop:20,
-        marginLeft:40,
-    },
-    list_text:{
-        fontSize:20,
-        fontWeight:'bold'
-    },
-    sub_text:{
-        fontSize:20,
-        fontWeight:'400',
-        color:'black'
-    }
+  return (
+  <ScrollView>
+        <View style={styles.button}>
+          <Button title="get Data" onPress={fetchData} />
+</View>
+  {data.length != 0 ? (
+        <View>
+          {data.map( (cms)=> (
+            
+            <View style={styles.list} >
+            <Text style={styles.list_text}>Movie</Text>
+              <Text>{cms.displayName}</Text>
+              <Text style={styles.list_text}>Subtitle</Text>
+              <Text style={styles.list_text}>{cms.data.subtitle}</Text>
+              <Text style={styles.list_text}>Abstract</Text>
+              <Text>{cms.data.abstract}</Text>
+             
+            </View>
+            
+          ))}
+        </View>
+      ) : null} 
 
-})
+      </ScrollView>
+     
+
+    
+  );
+};
+const styles = StyleSheet.create({
+  headline: {
+    fontSize: 40,
+    alignSelf: 'center',
+    marginTop: 40,
+    fontWeight: 'bold',
+    color: '#00A5BF',
+  },
+  button: {
+    padding: 30,
+
+    alignItems: 'center',
+  },
+  list: {
+    marginTop: 20,
+    marginLeft: 30,
+    marginRight:30,
+    marginBottom:10
+  },
+  list_text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  sub_text: {
+    fontSize: 20,
+    fontWeight: '400',
+    color: 'black',
+  },
+});
 export default Home;
